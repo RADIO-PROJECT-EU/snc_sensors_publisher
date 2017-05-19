d9829cc2a44fda5c008a49b6ae2a4751ff55228e
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-05-19 15:43:26 +0200

    Added events publisher

--- src/snc.py
+++ src/snc.py
@@ -17,10 +17,14 @@ def init():
     api_url = rospy.get_param('~api_url', 'https://dev.encontrol.io/api/')
     username = rospy.get_param('~username', 'gstavrinos')
     password = rospy.get_param('~password', 'test')
-    publish_topic = rospy.get_param('~publish_topic', 'snc_sensors/status')
-    interval = rospy.get_param('~interval', 60)
+    installationid = rospy.get_param('~installation_id', '00000000-0000-0000-0000-b827eb9a72ca')
+    userid = rospy.get_param('~user_id', 543)
+    sensor_topic = rospy.get_param('~sensor_topic', 'snc_sensors/status')
+    event_topic = rospy.get_param('~event_topic', 'snc_sensors/events')
+    interval = rospy.get_param('~interval', 10)
 
-    publisher = rospy.Publisher(publish_topic, SnCSensorsMsg, queue_size=1)
+    publisher = rospy.Publisher(sensor_topic, SnCSensorsMsg, queue_size=1)
+    publisher2 = rospy.Publisher(event_topic, SnCSensorsMsg, queue_size=1)
 
     s = requests.Session()
     auth = False
@@ -31,7 +35,7 @@ def init():
         except:
             print traceback.format_exc()
             time.sleep(10)
-    jsn =  json.loads(s.get(api_url + '/installations/00000000-0000-0000-0000-b827eb9a72ca/sensors?format=json').text)
+    jsn =  json.loads(s.get(api_url + '/installations/'+installationid+'/sensors?format=json').text)
     
     sensor_ids = []
     sensor_names = []
@@ -69,11 +73,36 @@ def init():
             msg.sensors = sensors
             publisher.publish(msg)
             #print datetime.datetime.now()
-            time.sleep(interval)
+
+            time.sleep(interval/2)
+
+            msg = SnCSensorsMsg()
+            events = []
+
+            jsn =  json.loads(s.get(api_url + '/users/'+str(userid)+'/events/').text)
+            jsn = jsn['Events']
+            cnt = 0
+            for j in jsn:
+                event = SnCSensor()
+                event.id = 'N/A'
+                event.name = j['Description']
+                event.status = j['Date']
+                event.value = 0
+                events.append(event)
+                # Add the five latest only, because the json response includes days of events!
+                cnt += 1
+                if cnt >= 5:
+                    break
+
+            msg.header.stamp = rospy.Time.now()
+            msg.sensors = events
+            publisher2.publish(msg)
+
+            time.sleep(interval/2)
         except:
             # Do we get here because of an expired session?
             print traceback.format_exc()
-	    try:
+            try:
                 s.post(api_url + 'auth?format=json&username=' + username + '&password=' + password)
             except:
                 print traceback.format_exc()
